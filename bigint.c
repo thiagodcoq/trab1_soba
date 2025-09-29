78606486e308474004542715d56f3d6f4373b378
Sofia porto <sofianporto@gmail.com>
2025-09-29 18:47:51 -0300

    Update bigint.c

--- bigint.c
+++ bigint.c
@@ -2,6 +2,15 @@
 /* Thiago D'Amico 2210799 3WC */
 
 #include "bigint.h"
-#include <stdioh.h>
+#include <stdio.h>
 #define NUM_BITS 128
 typedef unsigned char BigInt[NUM_BITS/8];
+
+void big_val(BigInt res, long val) { return; }
+void big_comp2(BigInt res, BigInt a) { return; }
+void big_sum(BigInt res, BigInt a, BigInt b) { return; }
+void big_sub(BigInt res, BigInt a, BigInt b) { return; }
+void big_mul(BigInt res, BigInt a, BigInt b) { return; }
+void big_shl(BigInt res, BigInt a, int n) { return; }
+void big_shr(BigInt res, BigInt a, int n) { return; }
+void big_sar(BigInt res, BigInt a, int n) { return; }
